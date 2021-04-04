bb9bd8129806c147ab9ac37afd98ca7421e26d21
Amila Samaranayake <samaranayake@axinom.com>
2021-04-05 01:33:14 +0530

    Initial Movie API structure fix 2

--- src/constants/moviesRestApi.constants.ts
+++ src/constants/moviesRestApi.constants.ts
@@ -1,3 +1,3 @@
 export const PORT = 9001;
-export const WELCOME_MESSAGE = "Welcome to PokeAPI REST by Nya ^^";
+export const WELCOME_MESSAGE = "Welcome to MoviesAPI REST by Amila";
 export const MONGO_URL = "mongodb://localhost:27017/Movies";
